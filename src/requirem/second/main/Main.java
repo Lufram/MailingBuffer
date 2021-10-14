5ecec79b700cec5d69f45e192e050dd2988cbbf8
Lufram <63366850+Lufram@users.noreply.github.com>
2021-10-15 00:56:44 +0200

    Update Main.java

--- src/requirem/second/main/Main.java
+++ src/requirem/second/main/Main.java
@@ -1,34 +1,55 @@
 package requirem.second.main;
 
 import requirem.second.Buffer;
+import requirem.second.Consumer;
+import requirem.second.Producer;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Buffer buffer = new Buffer();   //se crea un unico objeto Buffer (cola de los emails). Productores y Consumidores estan unidos a este mismo buffer.
+        // Crea el buffer
+        Buffer buffer = new Buffer();
+
+        //Poblamos de correos remitentes el array
+        String[] sender = new String[4];
+        sender[0] = "miriam.dpt-comercial@piramidgold.com";
+        sender[1] = "victor.dpt-comercial@piramidgold.com";
+        sender[2] = "alfonso.dpt-comercial@piramidgold.com";
+        sender[3] = "mar.dpt-comercial@piramidgold.com";
+        buffer.setSenders(sender);
+
+
+        //Poblamos de correos destinatariosla lista
+        buffer.addRecipients("rechulona88@gmail.com");
+        buffer.addRecipients("picachu88@gmail.com");
+        buffer.addRecipients("qrikverga@todoloqpuedas.com");
+        buffer.addRecipients("mamasita-rica@gmail.com");
+        buffer.addRecipients("nomecuentestuvida@gmail.com");
+
 
         //creamos 3 hilos productores
-        
-        Producer producer1 = new Producer(producer1, buffer);
-        Producer producer2 = new Producer(null, buffer);
-        Producer producer3 = new Producer(null, buffer);
+        Producer p1 = new Producer("producer1", buffer);
+        p1.setName(p1.getThreadName());
+        Producer p2 = new Producer("producer2", buffer);
+        p2.setName(p2.getThreadName());
+        Producer p3 = new Producer("producer3", buffer);
+        p3.setName(p3.getThreadName());
 
         //creamos 2 hilos consumidores
-        Consumer consumer1 = new Consumer(null, buffer);     
-        Consumer consumer2 = new Consumer(null, buffer);
-
-
+        Consumer c1 = new Consumer("consumer1", buffer);
+        Consumer c2 = new Consumer("consumer2", buffer);
 
         //arrancamos hilos Consumidores e hilos Productores
         p1.start();
         p2.start();
         p3.start();
+
         c1.start();
         c2.start();
 
-
-
+        System.out.println("termina el main");
     }
-
 }
