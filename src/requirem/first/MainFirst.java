3545bf81843ac254e5373e0eedee0b422ec9876f
Lufram <63366850+Lufram@users.noreply.github.com>
2021-10-15 21:35:56 +0200

    Update and rename Main.java to MainFirst.java

--- src/requirem/first/MainFirst.java
+++ src/requirem/first/MainFirst.java
@@ -2,7 +2,7 @@ package requirem.first;
 
 import java.util.Scanner;
 
-public class Main {
+public class MainFirst {
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
