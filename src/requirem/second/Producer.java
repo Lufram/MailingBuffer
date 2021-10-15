4bc82b973c2d1492c379252db983adaa7cce4a4e
Lufram <63366850+Lufram@users.noreply.github.com>
2021-10-15 16:28:23 +0200

    Update Producer.java

--- src/requirem/second/Producer.java
+++ src/requirem/second/Producer.java
@@ -1,35 +1,36 @@
 package requirem.second;
 
 public class Producer extends Thread {
-
-    private String threadName;
+    
+    private String producerName;
     private Buffer buffer;
+    
     // Constructor
-    public Producer (String threadName, Buffer buffer) {
+    public Producer (String producerName, Buffer buffer) {
         super();
-        this.threadName = threadName;
+        this.producerName = producerName;
         this.buffer = buffer;
     }
-
-    public String getThreadName() {
-        return threadName;
+    
+    // Getters y setters
+    public String getProducerName() {
+        return producerName;
     }
-
-    public void setThreadName(String threadName) {
-        this.threadName = threadName;
-    }
-
     public Buffer getBuffer() {
         return buffer;
     }
-
+    public void setProducerName(String producerName) {
+        this.producerName = producerName;
+    }
     public void setBuffer(Buffer buffer) {
         this.buffer = buffer;
     }
 
     @Override
     public void run() {
+        // Cada hilo producira 10 Emails
         for (i = 1 ; i <= 10 ; i++) {
+            // Crea un nuevo Email mediaten el metodo adEmail de Buffer
             buffer.addEmail();
         }
     }
