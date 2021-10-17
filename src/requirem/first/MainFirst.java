70aa9ce41a4bb0f4f785bd8eae2bf2f7ab829562
Lufram <63366850+Lufram@users.noreply.github.com>
2021-10-17 18:58:28 +0200

    Update MainFirst.java

--- src/requirem/first/MainFirst.java
+++ src/requirem/first/MainFirst.java
@@ -4,27 +4,33 @@ import java.util.Scanner;
 
 public class MainFirst {
 
-    public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        Long numbers[] = new Long[4];
+	public static void main(String[] args) {
+		
+		//Pedimos 4 veces numero por consola y los almacenamos en el array numbers
+		Scanner read = new Scanner(System.in);
+		Long numbers[] = new Long[4];
 
-        for (int i = 0 ; i <= 3 ; i++){
-            System.out.println("Introduzca un numero: ");
-            numbers[i] = read.nextLong();
-        }
+		for (int i = 0; i <= 3; i++) {
+			System.out.println("Introduzca un numero: ");
+			numbers[i] = read.nextLong();
+		}
 
-        NumberToCheck number1 = new NumberToCheck(numbers[0]);
-        number1.setName("thread-1");
-        NumberToCheck number2 = new NumberToCheck(numbers[1]);
-        number1.setName("thread-2");
-        NumberToCheck number3 = new NumberToCheck(numbers[2]);
-        number1.setName("thread-3");
-        NumberToCheck number4 = new NumberToCheck(numbers[3]);
-        number1.setName("thread-4");
+		//creamos 4 hilos y le intrucimos los numeros solicitados
+		NumberToCheck number1 = new NumberToCheck(numbers[0]);
+		number1.setName("thread-1");
+		NumberToCheck number2 = new NumberToCheck(numbers[1]);
+		number1.setName("thread-2");
+		NumberToCheck number3 = new NumberToCheck(numbers[2]);
+		number1.setName("thread-3");
+		NumberToCheck number4 = new NumberToCheck(numbers[3]);
+		number1.setName("thread-4");
 
-        number1.start();
-        number2.start();
-        number3.start();
-        number4.start();
-    }
+		//iniciamos los 4 hilos
+		number1.start();
+		number2.start();
+		number3.start();
+		number4.start();
+			
+		read.close();
+	}	 
 }
